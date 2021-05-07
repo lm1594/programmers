14b68afb227658e8ece67f864c135d83522344a1
이경민 <@host.docker.internal>
2021-05-07 22:19:00 +0900

    [Level2] 124 나라의 숫자

--- src/programmers/coding/test/Solution.java
+++ src/programmers/coding/test/Solution.java
@@ -8,7 +8,30 @@ package programmers.coding.test;
 public class Solution {
 	
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		System.out.println("답: " + solution(13));
 	}
+	
+	/**
+	 * 124 나라의 숫자
+	 * @param n
+	 * @return
+	 */
+	public static String solution(int n) {
+        String answer = "";
+        
+        while (n > 0) {
+        	
+        	// 3으로 나누었을 때 나머지가 0이면 4세팅 -> 124나라에는 0이라는 숫자가 없기 때문에
+        	if(n%3 == 0) {
+        		answer = "4"+answer;
+        		n = (n/3)-1;	// 자릿수가 바뀌지 않았기 때문에 -1 해줘야함.
+        	}else {	// 나머지 세팅
+        		answer = String.valueOf((n%3))+answer;
+            	n = n/3;   
+        	}
+        }
+        
+        return answer;
+    }
 
 }
