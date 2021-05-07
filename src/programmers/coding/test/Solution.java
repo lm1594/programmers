39ee37bc4923f4ff8c46eaa5b365cd95a280b6c2
이경민 <@host.docker.internal>
2021-05-07 21:24:59 +0900

    [프로그래머스] 초기 세팅

---
+++ src/programmers/coding/test/Solution.java
@@ -0,0 +1,14 @@
+package programmers.coding.test;
+
+/**
+ * 프로그래머스
+ * @author 이경민
+ *
+ */
+public class Solution {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+}
