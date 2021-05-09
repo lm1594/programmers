936e4c5975d62e2f29f5eb437f3dda068e00768d
이경민 <@host.docker.internal>
2021-05-09 14:52:33 +0900

    [Level2] 2017 팁스타운_짝지어 제거하기

--- src/programmers/coding/test/Solution.java
+++ src/programmers/coding/test/Solution.java
@@ -1,5 +1,7 @@
 package programmers.coding.test;
 
+import java.util.Stack;
+
 /**
  * 프로그래머스
  * @author 이경민
@@ -9,7 +11,8 @@ public class Solution {
 	
 	public static void main(String[] args) {
 		// System.out.println("답: " + solution1(13));
-		System.out.println("답: " + solution2("xababcdcdababcdcd"));
+		// System.out.println("답: " + solution2("xababcdcdababcdcd"));
+		System.out.println("답: " + solution3("baabaa"));
 	}
 	
 	/**
@@ -86,4 +89,41 @@ public class Solution {
         return answer;
     }
 
+	/**
+	 * 2017 팁스타운_짝지어 제거하기
+	 * @param s
+	 * @return
+	 */
+	public static int solution3(String s)
+    {
+        // 내가 만든 재귀 함수
+        // s = removeStr(s);
+        
+        // 모범 답안_Stack 자료형 이용
+		Stack<Character> stack = new Stack<Character>();
+		for (Character c : s.toCharArray()) {
+			if(!stack.isEmpty() && stack.peek() == c) {
+				stack.pop();
+			} else {
+				stack.push(c);
+			}
+		}
+        
+        return stack.isEmpty() ? 1 : 0;
+    }
+	// 내가 만든 재귀 함수
+	public static String removeStr(String s) {
+		
+		for (int i=0 ; i<=s.length()-2; i++) {
+			if(s.substring(i, i+1).equals(s.substring(i+1, i+2))) {
+				if (i == 0) {
+					return removeStr(s.substring(i+2));
+				} else {
+					return removeStr(s.substring(0,  i) + s.substring(i+2));
+				}
+			}
+		}
+		
+		return s;
+	} 
 }
