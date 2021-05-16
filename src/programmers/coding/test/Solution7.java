8e906ee1e36b0ed48a91459dd1f72bd5f738e5fe
이경민 <@host.docker.internal>
2021-05-16 17:39:23 +0900

    ..

--- src/programmers/coding/test/Solution7.java
+++ src/programmers/coding/test/Solution7.java
@@ -3,8 +3,6 @@ package programmers.coding.test;
 public class Solution7 {
 	/**
 	 * [Level2] Summer/Winter Coding(2019)_멀쩡한 사각형
-	 * @param numbers
-	 * @param target
 	 * @return
 	 */
 	public static long solution7(int w, int h) {
